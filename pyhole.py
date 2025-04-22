f8e396dbb0d7d7725dcdfb0967e3ce95a6f7f6e7
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2025-04-22 11:04:27 +0200

    Tidy up methods for authentication
    Improve documentation and print states more often.

--- pyhole.py
+++ pyhole.py
@@ -15,17 +15,26 @@ class Pihole:
 		self._headers = None
 
 	def is_auth_required(self):
+		"""
+		Check if authentication is required.
+
+		Returns: JSON object
+		"""
 		return requests.get(self.url + "/auth", headers=self._headers, verify=CERT_BUNDLE).json()
 
 	def authenticate(self):
 		"""
 		Creates a session token via the password provided in the config file.
+
+		Returns:
+		- None if successful
+		- JSON object otherwise
 		"""
 		try:
 			password = utils.get_config("password")
 		except KeyError:
 			print("No password provided in config file")
-			return
+			return {}
 
 		payload = {"password": password}
 		auth_request = requests.post(self.url + "/auth", json=payload, verify=CERT_BUNDLE)
@@ -34,27 +43,41 @@ class Pihole:
 			if auth_request.json()['session']['sid'] is None:
 				print("Authentication not required")
 				self._headers = None
-				return
+				return {}
 
 			self._headers = {
 				"X-FTL-SID": auth_request.json()['session']['sid'],
 				"X-FTL-CSRF": auth_request.json()['session']['csrf']
 			}
+			print("Authentication successful")
 		else:
 			self._headers = None
 			print("Authentication not successful")
-			return auth_request
+			return auth_request.json()
 
-	def delete_current_session(self):
+	def delete_current_session(self) -> None:
+		"""
+		Delete the current session.
+
+		Returns: None
+		"""
 		req = requests.delete(self.url + "/auth", headers=self._headers, verify=CERT_BUNDLE)
 		if req.status_code == 204:
 			print("Current session deleted")
-		else:
-			return req
+
+		if req.status_code == 401:
+			print("Authentication required")
+
+		if req.status_code == 404:
+			print("No active session")
 
 	def create_app_password(self):
 		"""
 		Creates a password to authenticate against the API instead of the user password. Overwrites the password defined in the config file.
+
+		Returns:
+		- None if successful
+		- JSON object otherwise
 		"""
 		req = requests.get(self.url + "/auth/app", headers=self._headers, verify=CERT_BUNDLE)
 		if req.status_code == 200:
@@ -74,17 +97,34 @@ class Pihole:
 			}
 
 			requests.patch(self.url + "/config", headers=self._headers, json=payload, verify=CERT_BUNDLE)
+			print("Password created")
 		else:
-			return req
+			return req.json()
 
 	def delete_session(self, id: int):
+		"""
+		Deletes the session with the given id.
+		"""
 		req = requests.delete(self.url + "/auth/session/" + str(id), headers=self._headers, verify=CERT_BUNDLE)
 		if req.status_code == 204:
 			print("Session deleted")
-		else:
-			return req
+
+		if req.status_code == 400:
+			print("Bad request")
+			return req.json()
+
+		if req.status_code == 401:
+			print("Authentication required")
+
+		if req.status_code == 404:
+			print("Session not found")
 
 	def get_sessions(self):
+		"""
+		Get a list of all sessions.
+
+		Returns: JSON object
+		"""
 		return requests.get(self.url + "/auth/sessions", headers=self._headers, verify=CERT_BUNDLE).json()
 
 
