f1e5eab9b8921235bcaa216723e126c34363d3c7
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2025-04-22 22:45:24 +0200

    Clean up code mess
    Does several things: fixes code issues, adds the group component, improves authentication workflow for exceeding maximum number of sessions.
    
    I know, not an optimal commit workflow.
    
    At least I have documented the changes here.

--- pyhole.py
+++ pyhole.py
@@ -13,8 +13,9 @@ class Pihole:
 
 		self.metrics = MetricAPI(self)
 		self.dns_filter = DnsFilterAPI(self)
-		self.clients = ClientAPI(self)
 		self.groups = GroupAPI(self)
+		self.domains = DomainAPI(self)
+		self.clients = ClientAPI(self)
 
 	def is_auth_required(self):
 		"""
@@ -28,9 +29,9 @@ class Pihole:
 		"""
 		Creates a session token via the password provided in the config file.
 
-		Returns:
+		:returns:
 		- None if successful
-		- JSON object otherwise
+		- JSON object
 		"""
 		try:
 			password = utils.get_config("password")
@@ -52,26 +53,33 @@ class Pihole:
 				"X-FTL-CSRF": auth_request.json()['session']['csrf']
 			}
 			print("Authentication successful")
+		elif auth_request.status_code == 429:
+			self._headers = None
+			print("Rate limit exceeded")
+			return auth_request.json()
 		else:
 			self._headers = None
 			print("Authentication not successful")
 			return auth_request.json()
 
-	def delete_current_session(self) -> None:
+	def delete_current_session(self) -> bool:
 		"""
 		Delete the current session.
 
-		Returns: None
+		:returns: bool
 		"""
 		req = requests.delete(self.url + "/auth", headers=self._headers, verify=CERT_BUNDLE)
 		if req.status_code == 204:
 			print("Current session deleted")
+			return True
 
 		if req.status_code == 401:
 			print("Authentication required")
 
 		if req.status_code == 404:
 			print("No active session")
+
+		return False
 
 	def create_app_password(self):
 		"""
@@ -137,6 +145,7 @@ class Pihole:
 		"""
 		return requests.get(self.url + "/auth/totp", headers=self._headers, verify=CERT_BUNDLE).json()
 
+
 class MetricAPI:
 	def __init__(self, pi):
 		self._pi = pi
@@ -160,7 +169,7 @@ class MetricAPI:
 		:param: Number of top clients. If set to 0, all clients will be returned.
 		:returns: JSON object
 		"""
-		return requests.get(self._pi._url + f"/history/clients?N={count}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
+		return requests.get(self._pi.url + f"/history/clients?N={count}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
 	def get_long_term_history(self, start: int, end: int):
 		"""
@@ -170,7 +179,7 @@ class MetricAPI:
 		:param: Unix timestamp from when the data should be requested
 		:returns: JSON object
 		"""
-		return requests.get(self._pi._url + f"/history/clients?from={start}&until={end}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
+		return requests.get(self._pi.url + f"/history/clients?from={start}&until={end}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
 	def get_long_term_client_history(self, start: int, end: int):
 		"""
@@ -180,10 +189,11 @@ class MetricAPI:
 		:param: Unix timestamp from when the data should be requested
 		:returns: JSON object
 		"""
-		return requests.get(self._pi._url + f"/history/database/clients?from={start}&until={end}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
+		return requests.get(self._pi.url + f"/history/database/clients?from={start}&until={end}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
-	def get_queries(self, options: dict):
+	def get_queries(self, options={}):
 		# TODO: Documentation?
+		# TODO: It is unclear if "from" and "until" are optional or required parameters
 		"""
 		Request query details.
 
@@ -199,13 +209,13 @@ class MetricAPI:
 
 		query_params = query_params.removesuffix("&")
 
-		return requests.get(self._pi._url + endpoint + query_params, headers=self._pi._headers, verify=CERT_BUNDLE).json()
+		return requests.get(self._pi.url + endpoint + query_params, headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
 	def get_suggestions(self):
 		"""
 		Get query filter suggestions suitable for _get\_queries_
 		"""
-		return requests.get(self._pi._url + "/queries/suggestions", headers=self._pi._headers, verify=CERT_BUNDLE).json()
+		return requests.get(self._pi.url + "/queries/suggestions", headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
 	def get_long_term_query_types(self, start: int, end: int):
 		"""
@@ -215,7 +225,7 @@ class MetricAPI:
 		:param: Unix timestamp from when the data should be requested
 		:returns: JSON object
 		"""
-		return requests.get(self._pi._url + f"/stats/database/query_types?from={start}&until={end}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
+		return requests.get(self._pi.url + f"/stats/database/query_types?from={start}&until={end}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
 	def get_database_summary(self, start: int, end: int):
 		"""
@@ -225,7 +235,7 @@ class MetricAPI:
 		:param: Unix timestamp from when the data should be requested
 		:returns: JSON object
 		"""
-		return requests.get(self._pi._url + f"/stats/database/summary?from={start}&until={end}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
+		return requests.get(self._pi.url + f"/stats/database/summary?from={start}&until={end}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
 	def get_long_term_top_clients(self, start: int, end: int, **kwargs):
 		"""
@@ -343,7 +353,12 @@ class DnsFilterAPI:
 	def __init__(self, pi):
 		self._pi = pi
 
-	def is_active(self) -> bool:
+	def is_active(self):
+		"""
+		Get current blocking state.
+
+		:returns: JSON object
+		"""
 		return requests.get(self._pi.url + "/dns/blocking", headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
 	def enable(self, timer: int):
@@ -387,14 +402,133 @@ class DnsFilterAPI:
 		return requests.post(self._pi.url + "/dns/blocking", json=payload, headers=self._pi._headers, verify=CERT_BUNDLE).json()
 
 
+class GroupAPI:
+	def __init__(self, pi):
+		self._pi = pi
+
+	def get_groups(self):
+		return requests.get(self._pi.url + "/groups", headers=self._pi._headers, verify=CERT_BUNDLE).json()
+
+	def get_group(self, name: str):
+		return requests.get(self._pi.url + f"/groups/{name}", headers=self._pi._headers, verify=CERT_BUNDLE).json()
+
+	def create_group(self, name: str, comment="", enabled=True, *names: str):
+		"""
+		Create a new group.
+
+		A "UNIQUE constraint failed" error indicates that a group with the same name already exists.
+
+		:param: name: Name of the group
+		:param: comment: Comment describing the group
+		:param: enabled: Whether the group is enabled or not
+		:returns: JSON object
+		"""
+		group = {
+			"name": name,
+			"comment": comment,
+			"enabled": enabled
+		}
+
+		if len(names) > 0:
+			group["name"] = names
+
+		return requests.post(self._pi.url + "/groups", json=group, headers=self._pi._headers, verify=CERT_BUNDLE).json()
+
+	def rename_group(self, old_name: str, new_name: str):
+		"""
+		Rename a group.
+
+		:param: old_name: Name the group currently has
+		:param: new_name: Name to change to
+		:returns: JSON object
+		"""
+		group = self.get_group(old_name)
+		group["name"] = new_name
+
+		return requests.put(self._pi.url + f"/groups/{old_name}", json=group, headers=self._pi._headers, verify=CERT_BUNDLE).json()
+
+	def enable_group(self, name: str):
+		"""
+		Enable group.
+
+		:param: name: Group name
+		:returns: JSON object
+		"""
+		group = self.get_group(name)
+		group["enabled"] = True
+
+		return requests.put(self._pi.url + f"/groups/{name}", json=group, headers=self._pi._headers, verify=CERT_BUNDLE).json()
+
+	def disable_group(self, name: str):
+		"""
+		Disable group.
+
+		:param: name: Group name
+		:returns: JSON object
+		"""
+		group = self.get_group(name)
+		group["enabled"] = False
+
+		return requests.put(self._pi.url + f"/groups/{name}", json=group, headers=self._pi._headers, verify=CERT_BUNDLE).json()
+
+	def delete_groups(self, *names) -> bool:
+		"""
+		Delete groups by name.
+
+		:param: name: Group names
+		:returns: bool
+		"""
+		groups = []
+		for name in names:
+			groups.append({"item": name})
+
+		req = requests.post(self._pi.url + "/groups:batchDelete", json=groups, headers=self._pi._headers, verify=CERT_BUNDLE)
+
+		if req.status_code == 204:
+			print("Groups deleted")
+			return True
+
+		if req.status_code == 401:
+			print("Authentication required")
+
+		if req.status_code == 404:
+			print("Groups not found")
+
+		return False
+
+	def delete_group(self, name: str) -> bool:
+		"""
+		Delete group by name.
+
+		:param: name: Group name
+		:returns: bool
+		"""
+		req = requests.delete(self._pi.url + f"/groups/{name}", headers=self._pi._headers, verify=CERT_BUNDLE)
+
+		if req.status_code == 204:
+			print("Group deleted")
+			return True
+
+		if req.status_code == 401:
+			print("Authentication required")
+
+		if req.status_code == 404:
+			print("Group not found")
+
+		return False
+
+
+class DomainAPI:
+	def __init__(self, pi):
+		self._pi = pi
+
+	def domain(self):
+		pass
+
+
 class ClientAPI:
 	def __init__(self, pi):
 		self._pi = pi
 
 	def get_url(self):
 		print(self._pi.url)
-
-
-class GroupAPI:
-	def __init__(self, pi):
-		self._pi = pi
